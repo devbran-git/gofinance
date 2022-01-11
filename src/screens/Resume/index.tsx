8ee82e9cfebd21cecf1394378d1a338c00aaadd7
devbran <evandro.brandao@lighthouseit.com.br>
2022-01-11 12:44:49 -0300

    feat(resume): date of chart customized

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,62 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { VictoryPie } from 'victory-native'
-import { HistoryCard } from '../../components/HistoryCard'
-import { categories } from '../../utils/categories'
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
+import { HistoryCard } from '../../components/HistoryCard';
+import { categories } from '../../utils/categories';
 
-import { Container, Header, Title, Content, ChartContainer } from './styles'
-import { RFValue } from 'react-native-responsive-fontsize'
-import { useTheme } from 'styled-components'
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
+} from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+import { addMonths, format, subMonths } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 interface TransactionData {
-  type: 'positive' | 'negative'
-  name: string
-  amount: string
-  category: string
-  date: string
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
 }
 
 interface TotalByCategory {
-  key: string
-  name: string
-  color: string
-  total: number
-  totalFormatted: string
-  percent: string
+  key: string;
+  name: string;
+  color: string;
+  total: number;
+  totalFormatted: string;
+  percent: string;
 }
 
 export function Resume() {
+  const theme = useTheme();
 
-  const theme = useTheme()
+  const [totalByCategories, setTotalByCategories] = useState<TotalByCategory[]>(
+    []
+  );
 
-  const [totalByCategories, setTotalByCategories] = useState<TotalByCategory[]>([])
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  function handleSelectedDate(action: 'prev' | 'next') {
+    if (action === 'next') {
+      setSelectedDate(addMonths(selectedDate, 1));
+    } else {
+      setSelectedDate(subMonths(selectedDate, 1));
+    }
+  }
 
   async function loadData() {
-    const dataKey = '@gofinance:transactions'
-    const response = await AsyncStorage.getItem(dataKey)
-    const responseFormatted = response ? JSON.parse(response) : []
+    const dataKey = '@gofinance:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
 
-    const expenses = responseFormatted.filter((expense: TransactionData) => expense.type === 'negative')
+    const expenses = responseFormatted.filter(
+      (expense: TransactionData) =>
+        expense.type === 'negative' &&
+        new Date(expense.date).getMonth() === selectedDate.getMonth() &&
+        new Date(expense.date).getFullYear() === selectedDate.getFullYear()
+    );
 
-    const expensesTotal = expenses.reduce((accumulator: number, expense: TransactionData) => {
-      return accumulator + Number(expense.amount)
-    }, 0)
+    const expensesTotal = expenses.reduce(
+      (accumulator: number, expense: TransactionData) => {
+        return accumulator + Number(expense.amount);
+      },
+      0
+    );
 
-    const totalByCategory: TotalByCategory[] = []
+    const totalByCategory: TotalByCategory[] = [];
 
-    categories.forEach(category => {
-      let categorySum = 0
+    categories.forEach((category) => {
+      let categorySum = 0;
 
       expenses.forEach((expense: TransactionData) => {
         if (expense.category === category.key) {
-          categorySum += Number(expense.amount)
+          categorySum += Number(expense.amount);
         }
-      })
+      });
 
       if (categorySum > 0) {
-        const totalFormatted = categorySum.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        const totalFormatted = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
 
-        const percent = `${(categorySum / expensesTotal * 100).toFixed(0)}%`
+        const percent = `${((categorySum / expensesTotal) * 100).toFixed(0)}%`;
 
         totalByCategory.push({
           key: category.key,
@@ -64,49 +98,60 @@ export function Resume() {
           color: category.color,
           total: categorySum,
           totalFormatted,
-          percent
-        })
+          percent,
+        });
       }
-    })
+    });
 
-    setTotalByCategories(totalByCategory)
+    setTotalByCategories(totalByCategory);
   }
 
   useEffect(() => {
-    loadData()
-  }, [])
+    loadData();
+  }, [selectedDate]);
   return (
     <Container>
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
       <Content>
+        <MonthSelect>
+          <MonthSelectButton onPress={() => handleSelectedDate('prev')}>
+            <MonthSelectIcon name="chevron-left" />
+          </MonthSelectButton>
+
+          <Month>{format(selectedDate, 'MMMM, yyyy', { locale: ptBR })}</Month>
+
+          <MonthSelectButton onPress={() => handleSelectedDate('next')}>
+            <MonthSelectIcon name="chevron-right" />
+          </MonthSelectButton>
+        </MonthSelect>
+
         <ChartContainer>
           <VictoryPie
             data={totalByCategories}
             x="percent"
             y="total"
-            colorScale={totalByCategories.map(category => category.color)}
-            labelRadius={100}
+            colorScale={totalByCategories.map((category) => category.color)}
+            labelRadius={99}
             style={{
               labels: {
                 fontSize: RFValue(18),
                 fontWeight: 'bold',
-                fill: theme.colors.shape
-              }
+                fill: theme.colors.shape,
+              },
             }}
           />
         </ChartContainer>
-        {
-          totalByCategories.map(category => <HistoryCard
+        {totalByCategories.map((category) => (
+          <HistoryCard
             key={category.key}
             title={category.name}
             amount={category.totalFormatted}
             color={category.color}
           />
-          )
-        }
+        ))}
       </Content>
     </Container>
-  )
+  );
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,10 +1,12 @@
-import styled from 'styled-components/native'
-import { RFValue } from 'react-native-responsive-fontsize'
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-`
+`;
 
 export const Header = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
@@ -13,19 +15,38 @@ export const Header = styled.View`
   align-items: center;
   justify-content: flex-end;
   padding-bottom: 10px;
-`
+`;
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(18)}px;
   color: ${({ theme }) => theme.colors.shape};
-  `
+`;
 
 export const Content = styled.ScrollView.attrs({
-  contentContainerStyle: { flexGrow: 1, padding: 24 }
-})``
+  contentContainerStyle: { flexGrow: 1, padding: 24 },
+  showsVerticalScrollIndicator: false,
+})``;
 
 export const ChartContainer = styled.View`
-width: 100%;
-align-items: center;
-`
+  width: 100%;
+  align-items: center;
+`;
+
+export const MonthSelect = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const MonthSelectButton = styled(BorderlessButton)``;
+
+export const MonthSelectIcon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+`;
+
+export const Month = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(20)}px;
+`;
